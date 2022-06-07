7732bbef5382bf8b139ca6a4bae4d466801ea134
Rafael Pereira <rafaelhpmg@gmail.com>
2022-06-06 23:14:55 -0300

    port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Server rodando em: http://localhost:3000');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
